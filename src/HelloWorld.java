eabf3e35f47896b454189e5d7ecf719ba485b129
rcortezk9 <rcortezk9@gmail.com>
2017-05-08 16:21:36 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * Created by renecortez on 5/8/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Pinnacle!");
+
+//        int myFavoritNumber = 7;
+//        System.out.println(myFavoritNumber);
+//
+//        String myString = "This is my String!";
+//        //myString = 'A';
+//        //myString = '3.14159';
+//        System.out.println(myString);
+//
+//        long myNumber = 123l;
+//        System.out.println(myNumber);
+    }
+}
+
+
---
+++ src/io/ConsoleExercises.java
@@ -0,0 +1,53 @@
+package io;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
+/**
+ * Created by renecortez on 5/8/17.
+ */
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age;
+
+//        System.out.println("What is your age?");
+//        age = sc.nextInt();
+//        System.out.println("Your age is: " + age);
+
+//        System.out.println("What are 3 of your favorite colors?");
+//        String firstColor = sc.nextLine();
+//        String secondColor = sc.nextLine();
+//        String thirdColor = sc.nextLine();
+//        System.out.println("Your first color is: " + firstColor);
+//        System.out.println("Your second color is: " + secondColor);
+//        System.out.println("Your third color is: " + thirdColor);
+
+//        System.out.println("What did you do yesterday");
+//        String yesterday = sc.nextLine();
+//        System.out.println("You entered: " + yesterday);
+
+        NumberFormat formatter = new DecimalFormat("#0.00");
+
+        System.out.println("Enter the length of the room?");
+        String length = sc.nextLine();
+        System.out.println("Enter the width of the room?");
+        String width = sc.nextLine();
+        System.out.println("Enter the height of the room?");
+        String height = sc.nextLine();
+
+        double lenghtResult = Double.parseDouble(length);
+        double widthResult = Double.parseDouble(width);
+        double area = lenghtResult * widthResult;
+        double perimeter = 2*lenghtResult + 2*widthResult;
+        double heightResult = Double.parseDouble(height);
+        double volume = lenghtResult*widthResult*heightResult;
+
+        System.out.println("The area is " + formatter.format(area));
+        System.out.println("The perimeter is " + formatter.format(perimeter));
+        System.out.println("The volume of the room is " + formatter.format(volume));
+
+    }
+}
