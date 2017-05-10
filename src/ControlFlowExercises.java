c00656f6e24d0af56a089b2f01a37828c62a6d77
rcortezk9 <rcortezk9@gmail.com>
2017-05-10 10:53:06 -0500

    for loop to do the fizz for mutliples of 3, buzz for the multiples of 5, and fizzbuzz for mutiples of 3 and 5.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,18 +3,27 @@
  */
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        int i = 5;
-        int count = 100;
+//        long i = 2;
+//        int count = 100;
 
+//        WHILE LOOP
 //        while (i <=15){
 //            System.out.println(i);
 //            i++;
 //        }
+
+//        DO WHILE LOOP
 //        do{
 //            System.out.println("count is: " + count);
 //            count -= 5;
 //        } while (count >= -10);
 
+//        do{
+//            System.out.println(i);
+//            i *= i;
+//        } while(i<=10000);
+
+//        FOR LOOP
 //        for(int i = 5; i<=15; i++){
 //            System.out.println("count is: " + i);
 //        }
@@ -23,8 +32,27 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for(int i = 100; i>=-10; i-=5){
-            System.out.println(i);
+//        for(int i = 100; i>=-10; i-=5){
+//            System.out.println(i);
+//        }
+//
+//        for(long i = 2; i<1000000; i *= i){
+//            System.out.println(i);
+//        }
+
+//        FizzBuzz
+        for(int i = 0; i<=100; i++){
+            if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+            } else if(i % (3*5) == 0){
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(String.valueOf(i));
+            }
+
+
         }
     }
 }
