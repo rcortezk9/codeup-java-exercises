c3ac00b6d25d594e38f94f5f46ae589b048ef91e
rcortezk9 <rcortezk9@gmail.com>
2017-05-10 16:14:55 -0500

    refactor

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -41,17 +41,17 @@ public class ControlFlowExercises {
 //        }
 
 //        FizzBuzz
-//        for(int i = 1; i<=100; i++){
-//            if(i % 3 == 0){
-//                System.out.println("Fizz");
-//            } else if(i % 5 == 0){
-//                System.out.println("Buzz");
-//            } else if(i % (3*5) == 0){
-//                System.out.println("FizzBuzz");
-//            } else {
-//                System.out.println(String.valueOf(i));
-//            }
-//
-//        }
+        for(int i = 1; i<=100; i++){
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
     }
 }
--- src/io/ControlFlowExercises_io.java
+++ src/io/ControlFlowExercises_io.java
@@ -8,33 +8,22 @@ import java.util.Scanner;
 public class ControlFlowExercises_io {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num;
-        int squareNum;
-        int cubeNum;
-        String heading1 = "number";
-        String heading2 = "square";
-        String heading3 = "cubed";
-        String line = "------";
+        String wantsToContinue;
 
+        do {
+            System.out.println("What number would you like to go up to?");
+            int num = input.nextInt();
 
-        System.out.println("What number would you like to go up to?");
-        num = input.nextInt();
-        System.out.println("");
-        System.out.println("Here is your table!");
-        System.out.println("");
-        System.out.printf("%-7s |", heading1);
-        System.out.printf("%-7s |", heading2);
-        System.out.printf("%-7s ", heading3);
-        System.out.printf("\n%-7s |", line);
-        System.out.printf("%-7s |", line);
-        System.out.printf("%-7s ", line);
+            System.out.println("Here is your table!");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
 
-        for(int i = 1; i<=num; i++){
-            squareNum = i * i;
-            cubeNum = i*i*i;
-            System.out.printf("\n%-7s |", i);
-            System.out.printf("%-7s |", squareNum);
-            System.out.printf("%-7s", cubeNum);
-        }
+            for (int i = 1; i <= num; i++) {
+// padding note: when using %s you'll have the not use spaces
+                System.out.printf("%-7d| %-8s| %d%n", i, i * i, i * i * i);
+            }
+            System.out.println("Do you want to continue? (y/n)");
+            wantsToContinue = input.next();//has to be .next()
+        } while(wantsToContinue.equalsIgnoreCase("y"));
     }
 }
