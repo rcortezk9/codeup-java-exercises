e815f8db17110da34aee7593c7e98e7000ec3a12
rcortezk9 <rcortezk9@gmail.com>
2017-05-10 14:22:00 -0500

    created a for loop that will get the user input, square it, cube it and display it in a table

--- src/io/ControlFlowExercises_io.java
+++ src/io/ControlFlowExercises_io.java
@@ -9,15 +9,32 @@ public class ControlFlowExercises_io {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num;
+        int squareNum;
+        int cubeNum;
+        String heading1 = "number";
+        String heading2 = "square";
+        String heading3 = "cubed";
+        String line = "------";
 
-        System.out.println("What number would you like to go up to? ");
+
+        System.out.println("What number would you like to go up to?");
         num = input.nextInt();
-        System.out.println("Here is your table");
-        System.out.println("number | square | cubed");
-        System.out.println("------ | ------ | -----");
+        System.out.println("");
+        System.out.println("Here is your table!");
+        System.out.println("");
+        System.out.printf("%-7s |", heading1);
+        System.out.printf("%-7s |", heading2);
+        System.out.printf("%-7s ", heading3);
+        System.out.printf("\n%-7s |", line);
+        System.out.printf("%-7s |", line);
+        System.out.printf("%-7s ", line);
 
         for(int i = 1; i<=num; i++){
-            System.out.println(i + "      | " + (i*=i) + "       | ");
+            squareNum = i * i;
+            cubeNum = i*i*i;
+            System.out.printf("\n%-7s |", i);
+            System.out.printf("%-7s |", squareNum);
+            System.out.printf("%-7s", cubeNum);
         }
     }
 }
