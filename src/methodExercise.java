a52c39c107673123ad5e8b34f3d0db8f641a0559
rcortezk9 <rcortezk9@gmail.com>
2017-05-11 16:17:00 -0500

    Set methods up

---
+++ src/methodExercise.java
@@ -0,0 +1,27 @@
+/**
+ * Created by renecortez on 5/11/17.
+ */
+public class methodExercise {
+
+    public static int addition() {
+        return 0;
+    }
+
+    public static double subtraction(){
+        return 0;
+    }
+
+    public static double multiplication(){
+        return 0;
+    }
+
+    public static double division(){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
