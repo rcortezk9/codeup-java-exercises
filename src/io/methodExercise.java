ccffc2cc1aa454abef1cc5a2d110c620c9b00ccc
rcortezk9 <rcortezk9@gmail.com>
2017-05-12 14:21:46 -0500

    added new class that prompts the user to input an number 1 to 10.  Prompt the user of the number inputed and figure the factorial of the number.
    added a do while loop that will complete the process ask if finished

--- src/io/methodExercise.java
+++ src/io/methodExercise.java
@@ -38,13 +38,6 @@ public class methodExercise {
         }
     }
 
-    public static long factorial(int n){
-        if (n <= 1){
-            return 1;
-        } else {
-            return (n * factorial(n - 1));
-        }
-    }
 
     public static void main(String[] args) {
         int min = 1;
@@ -57,7 +50,6 @@ public class methodExercise {
         System.out.println(multiply(num1, num2));
         System.out.println(divide(num1,num2));
         System.out.println(modulus(num1, num2));
-        System.out.println(factorial(num1));
 
     }
 
---
+++ src/io/moreMethodExercise.java
@@ -0,0 +1,51 @@
+package io;
+
+import java.util.Scanner;
+
+/**
+ * Created by renecortez on 5/12/17.
+ */
+public class moreMethodExercise {
+
+    public static int getInteger(int min, int max){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number between " + min + " and " + max + ": ");
+        int userInput = scan.nextInt();
+
+        if (userInput >= min && userInput <= max) {
+            System.out.println("You have selected " + userInput);
+            return userInput;
+        } else {
+            System.out.println("Invalid number, try again");
+            return getInteger(min, max);
+        }
+
+
+    }
+
+    public static long factorial(int n){
+        if (n <= 1){
+            return 1;
+        } else {
+            return (n * factorial(n - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        String wantsToContinue;
+
+        int min = 1;
+        int max = 10;
+
+        do {
+            int numberFactorial = getInteger(min, max);
+
+            System.out.println("The factorial of " + numberFactorial + " is " + factorial(numberFactorial));
+            System.out.println("Do you want to continue? (y/n)");
+            wantsToContinue = scan.next();
+
+        } while (wantsToContinue.equalsIgnoreCase("y"));
+    }
+}
