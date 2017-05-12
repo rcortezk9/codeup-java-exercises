58a8816f991669cf565b1a2c9fd4e9a3120b86bd
rcortezk9 <rcortezk9@gmail.com>
2017-05-12 09:21:44 -0500

    added method for a modulus

--- src/methodExercise.java
+++ src/methodExercise.java
@@ -4,6 +4,7 @@
 public class methodExercise {
 
     public static double add(double x, double y) {
+
         return x + y;
     }
 
@@ -15,15 +16,20 @@ public class methodExercise {
         return x * y;
     }
 
-    public static double divid(double x, double y){
+    public static double divide(double x, double y){
         return x/y;
+    }
+
+    public static double modulus(double x, double y){
+        return x % y;
     }
 
     public static void main(String[] args) {
         System.out.println(add(4,5));
         System.out.println(subtract(5,4));
         System.out.println(multiply(4,5));
-        System.out.println(divid(9,3));
+        System.out.println(divide(9,0));
+        System.out.println(modulus(4, 5));
 
     }
 
