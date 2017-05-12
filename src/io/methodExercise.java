2233fd56bf3406bccbafcbfaafef9ec547acab84
rcortezk9 <rcortezk9@gmail.com>
2017-05-12 11:13:19 -0500

    added method that would ask the user for a number that ranges from 1 to 10.
    if not with in that range will prompt user for another input
    
    added recursion that would figure the factorial of the user's input

---
+++ src/io/methodExercise.java
@@ -0,0 +1,65 @@
+package io;
+
+import java.util.Scanner;
+
+/**
+ * Created by renecortez on 5/11/17.
+ */
+public class methodExercise {
+
+    public static double add(double x, double y) {return x + y;}
+
+    public static double subtract(double x, double y){
+        return x - y;
+    }
+
+    public static double multiply(double x, double y){
+        return x * y;
+    }
+
+    public static double divide(double x, double y){
+        return x/y;
+    }
+
+    public static double modulus(double x, double y){
+        return x % y;
+    }
+
+    public static int getInteger(int min, int max){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number between " + min + " and " + max + ": ");
+        int userInput = scan.nextInt();
+
+        if(userInput >= min && userInput <= max){
+            return userInput;
+        } else {
+            System.out.println("Invalid number, try again");
+            return getInteger(min, max);
+        }
+    }
+
+    public static long factorial(int n){
+        if (n <= 1){
+            return 1;
+        } else {
+            return (n * factorial(n - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 10;
+        int num1 = getInteger(min, max);
+        int num2 = getInteger(min, max);
+
+        System.out.println(add(num1,num2));
+        System.out.println(subtract(num1, num2));
+        System.out.println(multiply(num1, num2));
+        System.out.println(divide(num1,num2));
+        System.out.println(modulus(num1, num2));
+        System.out.println(factorial(num1));
+
+    }
+
+
+}
