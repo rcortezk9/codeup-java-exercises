bed78bdab370204b184bf4014d6c60ee67bfd2c6
rcortezk9 <rcortezk9@gmail.com>
2017-05-19 11:41:17 -0500

    created Shape Test class w/ a main method create a variable of the type Rectangle named box1 and assign it a new instance of the Rectangle class with a width of 4 and a length of 5 verify that the getPerimeter and getArea methods return 18 and 20, respectively.

--- src/Java2/ShapeTest.java
+++ src/Java2/ShapeTest.java
@@ -1,10 +1,12 @@
 package Java2;
 
-/**
- * Created by renecortez on 5/19/17.
- */
+
 public class ShapeTest {
     public static void main(String[] args) {
+        Rectanlge box1 = new Rectanlge(5,4);
+
+        System.out.println("Area of the Rectangle is: " + box1.getArea());
+        System.out.println("Perimeter of the Rectangle is: " + box1.getPerimeter());
 
     }
 }
