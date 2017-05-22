c533f066700b88861480eaf11f85e044453d6888
rcortezk9 <rcortezk9@gmail.com>
2017-05-22 15:52:16 -0500

    Created a class named GradesApplication with a main method. Inside the main method, create a HashMap named students with the keys that are strings that student names, and values that are Student objects. Create at least 4 student objects with at least 3 grades each, and add them to the map.

--- src/Java2/GradesApplication.java
+++ src/Java2/GradesApplication.java
@@ -1,12 +1,33 @@
 package Java2;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by renecortez on 5/22/17.
  */
 public class GradesApplication {
     public static void main(String[] args) {
+        Map<String, Integer> students = new HashMap<>();
 
+        students.put("Bob", 90);
+        students.put("Bob", 80);
+        students.put("Bob", 97);
+        students.put("Bob", 91);
+
+        students.put("Tim", 99);
+        students.put("Tim", 89);
+        students.put("Tim", 95);
+        students.put("Tim", 83);
+
+        students.put("Paul", 88);
+        students.put("Paul", 88);
+        students.put("Paul", 88);
+        students.put("Paul", 88);
+
+        students.put("Chris", 90);
+        students.put("Chris", 90);
+        students.put("Chris", 90);
+        students.put("Chris", 90);
     }
 }
