822d99b4fde6778af8e238f9990bab78312417a5
rcortezk9 <rcortezk9@gmail.com>
2017-05-22 21:36:38 -0500

    created get student info method

--- src/Java2/Student.java
+++ src/Java2/Student.java
@@ -1,6 +1,8 @@
 package Java2;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by renecortez on 5/22/17.
@@ -21,6 +23,20 @@ public class Student {
     public void addGrade(int grade){
         grades.add(grade);
     }
+
+    public static void getStudentInfo(Map<String, Student>students, Scanner input){
+        String mapCheck = input.nextLine();
+
+        if (students.get(mapCheck) == null){
+            System.out.println("Sorry, no student found with the gihub username of \"" + mapCheck + "\".");
+            anotherSelection(students, input);
+        } else {
+            System.out.println("\nName: " + students.get(mapCheck).getName() + " - Github Username: " + mapCheck + " Current Average: " + students.get(mapCheck).getGradeAverage());
+            anotherSelectiono(students, input);
+        }
+    }
+
+
 
     public double getGradeAverage(){
         int sum = 0;
